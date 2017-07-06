c550934157f03bbe2b5ca9f6e4dd233eaa7feda8
Rositsa Koriykova <rosi.korikova@gmail.com>
2017-07-06 18:01:12 +0300

    modes

--- MyFit/src/components/modes/ModesPage.js
+++ MyFit/src/components/modes/ModesPage.js
@@ -1,11 +1,26 @@
 import React, {Component} from 'react'
 
-class ModesPage {
+class ModesPage extends Component {
     render() {
         return (
             <div>
                 <article>
-
+                    <h2>90 days diet</h2>
+                    <p> Everyday you eat different type of food group. The first food groups that you start with is
+                        foods
+                        rich in protein, followed by legumes and pulses, carbs and fruits which you rotate till the
+                        90-day of the diet schedule. It is essential to stick with the diet schedule in order to have
+                        visible results at the end of the day!
+                    </p>
+                    <button>Read more</button>
+                </article>
+                <article>
+                    <h2>1,350-calorie-a-day diet</h2>
+                    <p>Our 1,350-calorie-a-day diet features foods that are great at stopping that famished feeling.
+                        Plus, you’ll get 25 grams of slimming fiber a day. Just pick one breakfast, lunch, dinner, and
+                        snack each day. Stick to our diet and this workout plan for five weeks, and you’ll lose up to 15
+                        pounds.</p>
+                    <button>Read more</button>
                 </article>
 
             </div>
