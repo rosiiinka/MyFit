6b6ea60725a8882728e6727ac2e00ba2be1f8771
Rositsa Koriykova <rosi.korikova@gmail.com>
2017-07-06 17:32:42 +0300

    about us

--- MyFit/src/components/about/AboutPage.js
+++ MyFit/src/components/about/AboutPage.js
@@ -1,10 +1,27 @@
-import React, { Component } from 'react'
+import React, {Component} from 'react'
 
 class AboutPage extends Component {
-    render () {
+    render() {
         return (
             <div>
-                Some info of us
+                <div className="text-us">
+                    <h1>MyFit</h1>
+                    <p>Some text about us Some text about us Some text about us Some text about us Some text about us
+                        Some text about us</p>
+                    <p>Some text about us Some text about us Some text about us Some text about us Some text about us
+                        Some text about us</p>
+                    <p>Some text about us Some text about us Some text about us Some text about us Some text about us
+                        Some text about us</p>
+                    <p>Some text about us Some text about us Some text about us Some text about us Some text about us
+                        Some text about us</p>
+                </div>
+                <h2 className="founders">Founders</h2>
+                <hr/>
+                <div className="images">
+                    <img src="images/niki.jpg" alt="niki" className="img-circle"/>
+                    <img src="images/rosi.jpg" alt="rosi" className="img-circle"/>
+                    <img src="images/vasko.jpg" alt="vasko" className="img-circle"/>
+                </div>
             </div>
         )
     }
--- MyFit/src/components/contacts/ContactsPage.js
+++ MyFit/src/components/contacts/ContactsPage.js
@@ -12,8 +12,8 @@ export default class ContactsPage extends Component {
                     <div className="row">
                         <iframe 
                             src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d11734.271460542474!2d23.3540749!3d42.67051205!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbg!4v1499339179300" 
-                            width="400" 
-                            height="300" 
+                            width="400"
+                            height="300"
                             frameborder="0" 
                             allowfullscreen></iframe>
                     </div>
