3e1ebd5a1c351a8089b7d29d558508c5464dba7f
eqinox <eqinox@abv.bg>
2017-07-06 08:51:06 +0300

    Auth.js added

---
+++ MyFit/src/utilities/Auth.js
@@ -0,0 +1,21 @@
+import UserActions from '../actions/UserActions'
+import UserStore from '../stores/UserStore'
+
+class Auth {
+    static authenticate (token, username) {
+        window.localStorage.setItem('token', token)
+        window.localStorage.setItem('currentUser', username)
+    }
+
+    static isAuthenticated (username) {
+        let user = UserActions.getByUsername(username)
+        let token = window.localStorage.getItem('token')
+        if (user.token === token) {
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+export default Auth
